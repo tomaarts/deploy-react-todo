0686dc3bfcdb17c15de8da120c8afde6edc270d1
tomaarts <67033937+tomaarts@users.noreply.github.com>
2020-06-25 15:00:41 -0700

    Updates

---
+++ precache-manifest.593443e32d986fc3670ceb9e676247f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7036e85a67c4507ca61170cfe50ce6",
+    "url": "/deploy-react-todo/index.html"
+  },
+  {
+    "revision": "6c2e4d3e7a6b75cc2719",
+    "url": "/deploy-react-todo/static/css/main.68b784ae.chunk.css"
+  },
+  {
+    "revision": "941b4bb382392b266f90",
+    "url": "/deploy-react-todo/static/js/2.ece865e1.chunk.js"
+  },
+  {
+    "revision": "d3fc611ef7f627ba2af4ba9baec10348",
+    "url": "/deploy-react-todo/static/js/2.ece865e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2e4d3e7a6b75cc2719",
+    "url": "/deploy-react-todo/static/js/main.a0b1e57c.chunk.js"
+  },
+  {
+    "revision": "353dba1eeb7375e63991",
+    "url": "/deploy-react-todo/static/js/runtime-main.16be9d04.js"
+  }
+]);
